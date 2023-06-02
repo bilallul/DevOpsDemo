bb01826350e74e540d231c944d0c5240a7befec5
Lulzim Bilali <bilallul@students.zhaw.ch>
2023-06-02 18:48:17 +0200

    add TestWithNegativNumbers

--- backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTest.java
@@ -38,5 +38,25 @@ public class CalculatorTest {
         Assertions.assertThrows(IllegalArgumentException.class, () ->
                 Calculator.calculate(5, 0, "divide"));
     }
+
+    @Test
+    public void testCalculateAddNegativeNumbers() {
+        Assertions.assertEquals(-5, Calculator.calculate(-2, -3, "add"));
+    }
+
+    @Test
+    public void testCalculateSubtractNegativeNumbers() {
+        Assertions.assertEquals(1, Calculator.calculate(-2, -3, "subtract"));
+    }
+
+    @Test
+    public void testCalculateMultiplyNegativeNumbers() {
+        Assertions.assertEquals(6, Calculator.calculate(-2, -3, "multiply"));
+    }
+
+    @Test
+    public void testCalculateDivideNegativeNumbers() {
+        Assertions.assertEquals(2, Calculator.calculate(-6, -3, "divide"));
+    }
 }
 
